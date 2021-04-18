92ad953f27c3ca476f7a791d490bf8b1cfd56e8f
John (Jack) Meyer <jack10042@gmail.com>
2021-04-17 23:28:50 -0400

    Update Angular

--- src/app/solution-renderer/solution-renderer.component.spec.ts
+++ src/app/solution-renderer/solution-renderer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SolutionRendererComponent } from './solution-renderer.component';
 
@@ -6,7 +6,7 @@ describe('SolutionRendererComponent', () => {
   let component: SolutionRendererComponent;
   let fixture: ComponentFixture<SolutionRendererComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SolutionRendererComponent ]
     })
